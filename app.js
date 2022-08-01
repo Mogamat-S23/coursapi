d0ea5e732488f2057800f967f81a19698fbb0be1
Mogamat-S23 <saleemariefdien@gmail.com>
2022-08-01 11:57:17 +0200

    new change

---
+++ app.js
@@ -0,0 +1,77 @@
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+// let courses = [
+//     {
+//     id: 1, name: 'Software Engineering'
+//     },
+//     {
+//     id: 2, name: 'Web Development'
+//     },
+//     {
+//     id: 3, name: "Database Management"
+//     }
+//     ];
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method);
+
+// res.setHeader('content-Type', 'text/html');
+
+// let path = './views/'
+
+// if(req.url === '/'){
+//     path.at += 'index.html'
+//     res.write(JSON.stringify(courses))
+// }else{
+//     path += '404.html'
+// }
+
+
+// fs.readFile(path, (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         res.end();
+//     } else {
+//         // res.write(data);
+//         res.end(data);
+//     }
+// })
+
+// });
+
+// server.listen(3000, 'localhost', () => {
+//     console.log('listening for requests on port 3000')
+// });
+
+
+const express = require('express');
+const app = express();
+const port = parseInt(process.env.port) || 4000; 
+// env is envolment  
+let courses = [
+       {
+         id: 1, name: 'Software Engineering'
+        },
+        {
+        id: 2, name: 'Web Development'
+        },
+        {
+        id: 3, name: "Database Management"
+        }
+];
+
+app.listen(port, ()=> {
+    console.log(`Server is running at port ${port}`);
+})
+app.get('/', (req, res)=> {
+    res.send(courses);
+});
+app.get('/courses/:id', (req, res)=> {
+    const index = parseInt(req.params.id) - 1;
+    if(index < courses.length) {
+        res.send(courses[index]);
+    }else {
+        res.send('Course was not found');
+    }
+});
